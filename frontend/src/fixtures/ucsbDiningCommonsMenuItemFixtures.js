f9bdcc5982b415f8d6a095bad034dfb423c7164e
TomShangguan <2244741559b@gmail.com>
2024-11-02 17:03:51 -0700

    task 10 finish

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,31 +1,27 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneucsbDiningCommonsMenuItem: [
-      {
+    oneucsbDiningCommonsMenuItem:{
         id: 1,
         diningCommonsCode: "ortega",
         name: "pork",
         station:"not special",
-
       },
-    ],
-  
     threeucsbDiningCommonsMenuItems: [
       {
-        id: 2,
+        id: 1,
         diningCommonsCode: "dlg",
         name: "pork",
         station:"special",
       },
   
       {
-        id: 3,
+        id: 2,
         diningCommonsCode: "ortega",
         name: "chicken",
         station:"special",
       },
   
       {
-        id: 4,
+        id: 3,
         diningCommonsCode: "dlg",
         name: "beef",
         station:"no special",
