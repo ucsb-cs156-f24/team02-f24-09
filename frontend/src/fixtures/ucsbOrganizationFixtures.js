99c5555d072d4e9dbb76d6036cd455659c8b8b32
elijahelephant <elijahwilliams@ucsb.edu>
2024-11-05 14:22:30 -0800

    added fixture file

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,39 @@
+const ucsbOrganizationFixtures = {
+    oneOrganization: [
+      {
+        id: 1,
+        orgCode: "NPL",
+        orgTranslationShort: "NAPPING LEAGUE",
+        orgTranslation: "Professional Napping League at UCSB",
+        inactive: true,
+      },
+    ],
+  
+    threeOrganizations: [
+      {
+        id: 2,
+        orgCode: "PIZ",
+        orgTranslationShort: "PIZZA HUNTERS",
+        orgTranslation: "Pizza Hunting Society @ UCSB",
+        inactive: false,
+      },
+  
+      {
+        id: 3,
+        orgCode: "UFO",
+        orgTranslationShort: "UFO WATCHERS",
+        orgTranslation: "Student UFO Spotting Association",
+        inactive: false,
+      },
+  
+      {
+        id: 4,
+        orgCode: "SQU",
+        orgTranslationShort: "SQUIRREL CLUB",
+        orgTranslation: "Campus Squirrel Appreciation Club",
+        inactive: true,
+      },
+    ],
+  };
+  
+  export { ucsbOrganizationFixtures };
