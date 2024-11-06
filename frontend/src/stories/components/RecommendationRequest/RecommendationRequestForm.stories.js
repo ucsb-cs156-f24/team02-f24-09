2ff1bde1fd3b47d23fa8005376f89728520a98d1
李宏涛 <lihongtao@rihiroshitous-MacBook-Air.local>
2024-11-06 00:38:32 -0800

    fix by elint

--- frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
@@ -24,10 +24,10 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-  initialContents: RecommendationRequestForm.oneDate,
-  buttonLabel: "Update",
-  submitAction: (data) => {
-    console.log("Submit was clicked with data: ", data);
-    window.alert("Submit was clicked with data: " + JSON.stringify(data));
-  },
-};
+    initialContents: recommendationRequestFixtures.oneRequest,
+    buttonLabel: "Update",
+    submitAction: (data) => {
+      console.log("Submit was clicked with data: ", data);
+      window.alert("Submit was clicked with data: " + JSON.stringify(data));
+    },
+  };
