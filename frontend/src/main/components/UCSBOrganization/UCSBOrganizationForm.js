6f862ab5a290d6e519a53de784d06b61b1f8553b
elijahelephant <elijahwilliams@ucsb.edu>
2024-11-05 17:59:11 -0800

    finished form

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.js
@@ -3,86 +3,106 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 function OrganizationForm({
-  initialContents,
-  submitAction,
-  buttonLabel = "Create",
+    initialContents,
+    submitAction,
+    buttonLabel = "Create",
 }) {
-  // Stryker disable all
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({ defaultValues: initialContents || {} });
-  // Stryker restore all
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm({ defaultValues: initialContents || {} });
+    // Stryker restore all
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const testIdPrefix = "OrganizationForm";
+    const testIdPrefix = "OrganizationForm";
 
-  return (
-    <Form onSubmit={handleSubmit(submitAction)}>
-      {initialContents && (
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="id">Id</Form.Label>
-          <Form.Control
-            data-testid={testIdPrefix + "-id"}
-            id="id"
-            type="text"
-            {...register("id")}
-            value={initialContents.id}
-            disabled
-          />
-        </Form.Group>
-      )}
+    return (
+        <Form onSubmit={handleSubmit(submitAction)}>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="orgCode">orgCode</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "orgCode"}
+                    id="orgCode"
+                    type="text"
+                    {...register("orgCode", {
+                        required: "orgCode is required.",
+                        maxLength: {
+                            value: 10,
+                            message: "Max length 10 characters",
+                        },
+                    })}
+                    isInvalid={Boolean(errors.orgCode)}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.orgCode?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="name">Name</Form.Label>
-        <Form.Control
-          data-testid={testIdPrefix + "-name"}
-          id="name"
-          type="text"
-          isInvalid={Boolean(errors.name)}
-          {...register("name", {
-            required: "Name is required.",
-            maxLength: {
-              value: 30,
-              message: "Max length 30 characters",
-            },
-          })}
-        />
-        <Form.Control.Feedback type="invalid">
-          {errors.name?.message}
-        </Form.Control.Feedback>
-      </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="OrgTranslationShort">OrgTranslationShort</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-OrgTranslationShort"}
+                    id="OrgTranslationShort"
+                    type="text"
+                    isInvalid={Boolean(errors.orgTranslationShort)}
+                    {...register("orgTranslationShort", {
+                        required: "OrgTranslationShort is required.",
+                        maxLength: {
+                            value: 30,
+                            message: "Max length 30 characters",
+                        },
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.orgTranslationShort?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="description">Description</Form.Label>
-        <Form.Control
-          data-testid={testIdPrefix + "-description"}
-          id="description"
-          type="text"
-          isInvalid={Boolean(errors.description)}
-          {...register("description", {
-            required: "Description is required.",
-          })}
-        />
-        <Form.Control.Feedback type="invalid">
-          {errors.description?.message}
-        </Form.Control.Feedback>
-      </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="OrgTranslation">OrgTranslation</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-OrgTranslation"}
+                    id="OrgTranslation"
+                    type="text"
+                    isInvalid={Boolean(errors.orgTranslation)}
+                    {...register("orgTranslation", {
+                        required: "OrgTranslation is required.",
+                        maxLength: {
+                            value: 30,
+                            message: "Max length 30 characters",
+                        },
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.orgTranslation?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
 
-      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
-        {buttonLabel}
-      </Button>
-      <Button
-        variant="Secondary"
-        onClick={() => navigate(-1)}
-        data-testid={testIdPrefix + "-cancel"}
-      >
-        Cancel
-      </Button>
-    </Form>
-  );
+            <Form.Group className="mb-3">
+                <Form.Check
+                    type="checkbox"
+                    label="Inactive"
+                    id="inactive"
+                    {...register("inactive")}
+                    data-testid={testIdPrefix + "-inactive"}
+                />
+            </Form.Group>
+
+            <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+                {buttonLabel}
+            </Button>
+            <Button
+                variant="Secondary"
+                onClick={() => navigate(-1)}
+                data-testid={testIdPrefix + "-cancel"}
+            >
+                Cancel
+            </Button>
+        </Form>
+    );
 }
 
 export default OrganizationForm;
