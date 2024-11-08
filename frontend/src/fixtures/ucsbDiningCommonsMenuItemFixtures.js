ca179fd24553f0fe65f556b400a02144d4eb60ce
TomShangguan <2244741559b@gmail.com>
2024-11-07 16:55:29 -0800

    TASK12

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,33 +1,32 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneucsbDiningCommonsMenuItem:{
-        id: 1,
-        diningCommonsCode: "ortega",
-        name: "pork",
-        station:"not special",
-      },
-    threeucsbDiningCommonsMenuItems: [
-      {
-        id: 1,
-        diningCommonsCode: "dlg",
-        name: "pork",
-        station:"special",
-      },
-  
-      {
-        id: 2,
-        diningCommonsCode: "ortega",
-        name: "chicken",
-        station:"special",
-      },
-  
-      {
-        id: 3,
-        diningCommonsCode: "dlg",
-        name: "beef",
-        station:"no special",
-      },
-    ],
-  };
-  
-  export { ucsbDiningCommonsMenuItemFixtures };
-  
+  oneucsbDiningCommonsMenuItem: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "pork",
+    station: "not special",
+  },
+  threeucsbDiningCommonsMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "dlg",
+      name: "pork",
+      station: "special",
+    },
+
+    {
+      id: 2,
+      diningCommonsCode: "ortega",
+      name: "chicken",
+      station: "special",
+    },
+
+    {
+      id: 3,
+      diningCommonsCode: "dlg",
+      name: "beef",
+      station: "no special",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.js
@@ -1,6 +1,5 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-
 export default function UCSBDiningCommonsMenuItemCreatePage() {
   // Stryker disable all : placeholder for future implementation
   return (
